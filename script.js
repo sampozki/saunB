ae0647ab4e3d307d92e9b84d340be36548c1a304
Sampo Pelto <pelto.sampo@gmail.com>
2020-10-24 01:13:20 +0300

    meta-tag

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-let newestnum;
-let currentnum;
-
 $(function(){
     gettemp('https://thingspeak.com/channels/113387/field/1.json');
 })
 
 function gettemp(url){
     $.get(url, function(data){
-        //temp
-        //timestamp
         let last_data = new Date(data.feeds[data.feeds.length-1].created_at)
-
         let timestamp = last_data.getDate()+1 + "."+ (last_data.getMonth()+1) + ". klo " + last_data.getHours() + ":" + last_data.getMinutes()
 
         $('#temp').text("Lämpötila: "+ data.feeds[data.feeds.length-1].field1 + "°C");
         $('#timestamp').text("Viimeisin data: " + timestamp);
+
+        $("meta[property='og\\:title']").attr("content", "Lämpötila: "+ data.feeds[data.feeds.length-1].field1 + "°C");
+        $("meta[property='og\\:description']").attr("content", "Viimeisin data: " + timestamp);
+
 
     });
 };
